7209523e6bcfed31c7562f6734db30f0b53192ca
Guillermo Hasbun <hasbun.guillermo@gmail.com>
2023-12-18 09:49:13 -0500

    do some cleaning

---
+++ public/src/components/component.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+const Component = (props) => {
+  //hooks go here
+
+  //functions and styling should go here
+
+  // return the function
+  return (
+    <div>Hi! I'm a component!</div>
+  )
+}
+
+export default Component;
